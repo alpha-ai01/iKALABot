f0038757a6d6878c1a402d1f775dd0f510b2b885
YourName <your_email@example.com>
2026-08-03 10:46:23 +0700

    Fix: Add GET/HEAD methods for UptimeRobot & Enforce Port 10000

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import http.server
 import socketserver
 import threading
 
-PORT = int(os.environ.get("PORT", 8080))
+PORT = int(os.environ.get("PORT", 10000))"PORT", 8080))
 
 class MyHandler(http.server.SimpleHTTPRequestHandler):
     def do_GET(self):
